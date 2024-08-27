1ebad280fe6f533e73ebff0ed70fa710e4973b4f
JuanEstebanChacon <est.juane.chacon@unimilitar.edu.co>
2024-08-27 15:23:00 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,15 @@
+//crear una clase llamada triangulo con atributos base y altura, aplicar encapsulamiento y los metodos apropiados para iniccializar o crear objetos con sus respectivos atributos
+
+class triangulo {
+
+    public static void main(String[] args) {
+       
+        int base = 5;
+        int altura = 10;
+        int area = 0;
+        area = (base * altura)/ 2;
+        System.out.println(area);
+        
+    }
+}
+
